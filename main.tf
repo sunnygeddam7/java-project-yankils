53d51c3215575c2d406859c6b0e7f5444af75a1a
Benjili Geddam <85113982+sunnygeddam7@users.noreply.github.com>
2024-12-13 18:30:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,40 @@
 provider "aws" {
-  region = "us-east-1" 
+  region = "ap-southeast-2" 
 }
 
+resource "aws_security_group" "ec2_security_group" {
+  name        = "ec2_security_group"
+  description = "Allow SSH and HTTP traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"          
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-005fc0f236362e99f"  
+  ami           = "ami-040e71e7b8391cae4"  
   instance_type = "t2.micro"               
+  key_name      = jenkins
+  security_groups = [aws_security_group.ec2_security_group.name] 
 
   tags = {
     Name = "MyFirstEC2"  
