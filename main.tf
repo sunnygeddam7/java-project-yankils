20f1664b0cd327e1daf2c55bb30f58ab76aba0fe
Benjili Geddam <85113982+sunnygeddam7@users.noreply.github.com>
2024-12-13 18:32:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "ec2_security_group" {
 resource "aws_instance" "my_ec2" {
   ami           = "ami-040e71e7b8391cae4"  
   instance_type = "t2.micro"               
-  key_name      = jenkins
+  key_name      = "jenkins"
   security_groups = [aws_security_group.ec2_security_group.name] 
 
   tags = {
