2eb7340d32857440d4a4671b8e758ccff3b6b9f1
Benjili Geddam <85113982+sunnygeddam7@users.noreply.github.com>
2024-12-13 18:10:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-040e71e7b8391cae4"  
+  ami           = "ami-005fc0f236362e99f"  
   instance_type = "t2.micro"               
 
   tags = {
